84cc20d75c7de4a19fede38b8b545ff077d74f10
Alan Deitch <alandeitch@gmail.com>
2014-03-15 13:13:27 -0500

    The Greatest Television Show bracket

---
+++ bracket.php
@@ -0,0 +1,374 @@
+<?php
+
+$page_file = "";
+$page_title = "";
+
+require('functions/main_fns.php');
+require('partials/_header.php'); ?>
+
+<div id="container">
+  <div class="row">
+    <h2>The Greatest Television Show from the last 35 years</h2>
+    <div class="tweleve columns centered">
+      <div id="bracket">
+        <div id="region_1">
+          <div class="round1"><div class="match left" id="match1">
+              <dl>
+              <dt class="band1"><span class="seed">1</span>TV SHOW</dt><dt class="band2"><span class="seed">16</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match2">
+              <dl>
+              <dt class="band1"><span class="seed">8</span>TV SHOW</dt><dt class="band2"><span class="seed">9</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match3">
+              <dl>
+              <dt class="band1"><span class="seed">5</span>TV SHOW</dt><dt class="band2"><span class="seed">12</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match4">
+              <dl>
+              <dt class="band1"><span class="seed">4</span>TV SHOW</dt><dt class="band2"><span class="seed">7</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match5">
+              <dl>
+              <dt class="band1"><span class="seed">6</span>TV SHOW</dt><dt class="band2"><span class="seed">11</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match6">
+              <dl>
+              <dt class="band1"><span class="seed">3</span>TV SHOW</dt><dt class="band2"><span class="seed">14</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match7">
+              <dl>
+              <dt class="band1"><span class="seed">7</span>TV SHOW</dt><dt class="band2"><span class="seed">10</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match8">
+              <dl>
+              <dt class="band1"><span class="seed">2</span>TV SHOW</dt><dt class="band2"><span class="seed">15</span>TV SHOW</dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round2"><div class="match left" id="match33">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match34">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match35">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match36">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round3"><div class="match left" id="match49">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match50">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round4"><div class="match left" id="match57">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          </div>
+          <div id="region_2">
+          <div class="round1"><div class="match left" id="match1">
+              <dl>
+              <dt class="band1"><span class="seed">1</span>TV SHOW</dt><dt class="band2"><span class="seed">16</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match2">
+              <dl>
+              <dt class="band1"><span class="seed">8</span>TV SHOW</dt><dt class="band2"><span class="seed">9</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match3">
+              <dl>
+              <dt class="band1"><span class="seed">5</span>TV SHOW</dt><dt class="band2"><span class="seed">12</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match4">
+              <dl>
+              <dt class="band1"><span class="seed">4</span>TV SHOW</dt><dt class="band2"><span class="seed">7</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match5">
+              <dl>
+              <dt class="band1"><span class="seed">6</span>TV SHOW</dt><dt class="band2"><span class="seed">11</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match6">
+              <dl>
+              <dt class="band1"><span class="seed">3</span>TV SHOW</dt><dt class="band2"><span class="seed">14</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match7">
+              <dl>
+              <dt class="band1"><span class="seed">7</span>TV SHOW</dt><dt class="band2"><span class="seed">10</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match8">
+              <dl>
+              <dt class="band1"><span class="seed">2</span>TV SHOW</dt><dt class="band2"><span class="seed">15</span>TV SHOW</dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round2"><div class="match left" id="match33">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match34">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match35">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match36">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round3"><div class="match left" id="match49">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match50">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round4"><div class="match left" id="match57">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          </div>
+          <div id="region_3">
+          <div class="round1"><div class="match left" id="match1">
+              <dl>
+              <dt class="band1"><span class="seed">1</span>TV SHOW</dt><dt class="band2"><span class="seed">16</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match2">
+              <dl>
+              <dt class="band1"><span class="seed">8</span>TV SHOW</dt><dt class="band2"><span class="seed">9</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match3">
+              <dl>
+              <dt class="band1"><span class="seed">5</span>TV SHOW</dt><dt class="band2"><span class="seed">12</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match4">
+              <dl>
+              <dt class="band1"><span class="seed">4</span>TV SHOW</dt><dt class="band2"><span class="seed">7</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match5">
+              <dl>
+              <dt class="band1"><span class="seed">6</span>TV SHOW</dt><dt class="band2"><span class="seed">11</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match6">
+              <dl>
+              <dt class="band1"><span class="seed">3</span>TV SHOW</dt><dt class="band2"><span class="seed">14</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match7">
+              <dl>
+              <dt class="band1"><span class="seed">7</span>TV SHOW</dt><dt class="band2"><span class="seed">10</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match8">
+              <dl>
+              <dt class="band1"><span class="seed">2</span>TV SHOW</dt><dt class="band2"><span class="seed">15</span>TV SHOW</dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round2"><div class="match left" id="match33">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match34">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match35">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match36">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round3"><div class="match left" id="match49">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match50">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round4"><div class="match left" id="match57">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          </div>
+          <div id="region_4">
+          <div class="round1"><div class="match left" id="match1">
+              <dl>
+              <dt class="band1"><span class="seed">1</span>TV SHOW</dt><dt class="band2"><span class="seed">16</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match2">
+              <dl>
+              <dt class="band1"><span class="seed">8</span>TV SHOW</dt><dt class="band2"><span class="seed">9</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match3">
+              <dl>
+              <dt class="band1"><span class="seed">5</span>TV SHOW</dt><dt class="band2"><span class="seed">12</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match4">
+              <dl>
+              <dt class="band1"><span class="seed">4</span>TV SHOW</dt><dt class="band2"><span class="seed">7</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match5">
+              <dl>
+              <dt class="band1"><span class="seed">6</span>TV SHOW</dt><dt class="band2"><span class="seed">11</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match6">
+              <dl>
+              <dt class="band1"><span class="seed">3</span>TV SHOW</dt><dt class="band2"><span class="seed">14</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match7">
+              <dl>
+              <dt class="band1"><span class="seed">7</span>TV SHOW</dt><dt class="band2"><span class="seed">10</span>TV SHOW</dt>
+          </dl>
+          </div>
+          <div class="match left" id="match8">
+              <dl>
+              <dt class="band1"><span class="seed">2</span>TV SHOW</dt><dt class="band2"><span class="seed">15</span>TV SHOW</dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round2"><div class="match left" id="match33">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match34">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match35">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match36">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round3"><div class="match left" id="match49">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match50">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          <div class="round4"><div class="match left" id="match57">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+
+          </div>
+          </div>
+          <div id="region_5"><div class="match right" id="match61">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match left" id="match62">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+          <div class="match" id="match63">
+              <dl>
+              <dt class="band1"><span class="seed"></span></dt><dt class="band2"><span class="seed"></span></dt>
+          </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php require('partials/_footer.php'); ?>
